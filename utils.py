d9ae3b82e9546d6da378ea6f20f639e9076cb827
chingdae1 <chingdae0820@hanmail.net>
2018-02-07 13:42:41 +0900

    prac

--- utils.py
+++ utils.py
@@ -94,12 +94,12 @@ def train_generator(train_batch_size, input_dim):
 
 def save_wav_to_arr(data_dir):
     # VCTK -> 44257 files
-    all_files = glob(os.path.join(data_dir, 'wav48/*/*wav'))
+    all_files = glob(os.path.join(data_dir, '*wav'))
 
     # For test, get sample
-    # all_files = glob(os.path.join(data_dir, '*wav'))
+    # all_files = glob(os.path.join('./data/', '*wav'))
 
-    output_dir = './audio_vector/'
+    output_dir = '../VCTK_audio_vector/'
     one_hot_list = []
     file_name_list = []
 
@@ -111,27 +111,46 @@ def save_wav_to_arr(data_dir):
         file_name_list.append(file_name)
         print('load_data complete : ' + file_name)
 
-    one_hot_list = pad_sequences(one_hot_list, padding='pre')
+
+    print('pad_sequences start.')
+    one_hot_list = pad_sequences(one_hot_list, maxlen=300000, padding='pre')
     i = 0
+    print('pad_sequences end.')
 
     for one_hot in one_hot_list:
         np.save(output_dir + file_name_list[i], one_hot)
         print('save file : ' + file_name_list[i])
         i = i + 1
 
+    print('save_wav_to_arr done.')
+
 def downsampling(data_dir, file_name, downsample_output_dir):
     y, sr = librosa.load(data_dir+file_name, sr=16000)
     librosa.output.write_wav(downsample_output_dir+file_name, y, sr)
 
 if __name__ == '__main__':
-    data_dir = '../VCTK-Corpus/'
+    data_dir = '../down_VCTK/'
     # save_wav_to_arr(data_dir)
     # sample = './data/'
     # file_name = 'p227_001.wav'
-    downsample_output_dir = './data_downsampling/'
+    # downsample_output_dir = './data_downsampling/'
+    #
+    # all_files = glob(os.path.join(data_dir, 'wav48/*/*wav'))
+    # for file in all_files:
+    #     file_name = file.split('/')[-1]
+    #     downsampling(data_dir, file_name, downsample_output_dir)
+    #     print('downsampling complete : ' + file)
 
-    all_files = glob(os.path.join(data_dir, 'wav48/*/*wav'))
-    for file in all_files:
-        file_name = file.split('/')[-1]
-        downsampling(data_dir, file_name, downsample_output_dir)
-        print('downsampling complete : ' + file)
+    save_wav_to_arr(data_dir)
+'''
+TODO list
+오디오 다운샘플링 끝나면 save_wav_to_arr 에 파일 경로 같은거 수정해서
+다운샘필링된 놈들 패딩된 넘파이 어레이로 저장 (그전에 패딩 같은거 잘 되는지 확인)
+
+위의 것들 서버에서 돌리는 동안 train_generator 작성
+
+다 되면 train
+train 하는 동안 generation 코드 작성
+
+다 되면 generate!
+'''
