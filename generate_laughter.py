7f87db6ff9636549e76a0d885ef5210d514d8ca3
chingdae1 <chingdae0820@hanmail.net>
2018-02-20 13:37:26 +0900

    prac

--- generate_laughter.py
+++ generate_laughter.py
@@ -1,4 +1,4 @@
-from utils import q_to_one_hot, sample, onehot_to_wave
+from utils import q_to_one_hot, sample, onehot_to_wave, load_audio, mu_quantize
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 from wavenet import build_model
@@ -10,16 +10,17 @@ log 하고 weight 파일 이름 바꿔서 log_and_weight 에 옮겨주고
 그리고 나서 아래 파라미터 이름 정해주고 돌릴 것.
 '''
 sr = 16000
-sec = 2
+sec = 3
 sample_len = 16000
 input_dim = 256
-new_wave_name = './generation/laughter_ac21_50_172000'
-audio_file = '50_172000'
+new_wave_name = './generation/laughter_ac23_50_172000'
+audio_file = '30_100000'
 seed_audio_path = '../laughter_audio_vector/' + audio_file + '.npy'
 weight_path = './laughter_wavenet_weight.hdf5'
 dilation_factor = [1,2,4,8,16,32,64,128,256,512,
                    1,2,4,8,16,32,64,128,256,512,
                    1,2,4,8,16,32,64,128,256,512]
+impulse = {0: './impulse/impulse.wav'}
 
 generated_sample = np.load(seed_audio_path)
 generated_sample = generated_sample.tolist()
@@ -35,14 +36,36 @@ model = build_model(sample_len, dilation_factor)
 model.load_weights(weight_path)
 
 generation_step = sr * sec
+prev_sample = -1
+equal_cnt = 0
+impulse_idx = 0
 
 for i in range(generation_step):
     preds = model.predict(np.expand_dims(pred_seed, 0))  # prediction with the model
     sampled = sample(preds[0][-1])  # multinomial sampling
+    # To prevent dead silence.
+    if sampled == prev_sample:
+        equal_cnt += 1
+    else:
+        equal_cnt = 0
+
     sampled_onehot = np.zeros([1, 1, input_dim])
     sampled_onehot[0][0][sampled] = 1  # make the sample into onehot
     generated_sample = np.append(generated_sample, sampled_onehot, axis=1)  # append generated sample
     pred_seed = generated_sample[0][i + 1:i + 1 + sample_len]  # make new seed as generation input
+    if equal_cnt > 1000:
+        impulse_audio = load_audio(impulse[impulse_idx])
+        impulse_audio = mu_quantize(impulse_audio, input_dim)
+        impulse_audio = impulse_audio[:1000]
+        impulse_audio = q_to_one_hot(impulse_audio, input_dim)
+        for j in range(1000):
+            pred_seed[j+15000] = impulse_audio[j]
+        print('Inject impulse.')
+        if impulse_idx == len(impulse) - 1:
+            impulse_idx = 0
+        else:
+            impulse_idx += 1
+
     print('generated %ith sample ==> %i' % ((i + 1), sampled), end='\r')
 
 # Save generated samples as a flie
